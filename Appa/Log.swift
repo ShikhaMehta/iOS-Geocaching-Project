559c4ab1b0af499e03cd9382629f46ae6f6aeb1a
Tyler Brockett <tylerbrockett@gmail.com>
2016-03-26 13:02:08 -0700

    Updated Storyboard and Core Data

---
+++ Appa/Log.swift
@@ -0,0 +1,17 @@
+//
+//  Log.swift
+//  Appa
+//
+//  Created by Tyler Brockett on 3/26/16.
+//  Copyright © 2016 ASU. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Log: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
