d086467e44f80431dfa08d2a2066b3616af451bf
Tyler Brockett <tylerbrockett@gmail.com>
2016-03-26 22:51:19 -0700

    Added SearchViewController back into project

--- Appa/Appa/SearchViewController.swift
+++ Appa/Appa/SearchViewController.swift
@@ -2,13 +2,146 @@
 //  SearchViewController.swift
 //  Appa
 //
-//  Created by Tyler Brockett on 3/26/16.
+//  Created by tam le on 3/25/16.
 //  Copyright © 2016 ASU. All rights reserved.
 //
 
-import UIKit
 import Foundation
+import UIKit
+import CoreData
+import MapKit
+import CoreLocation
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UINavigationControllerDelegate, CLLocationManagerDelegate, NSFetchedResultsControllerDelegate {
+    
+    
+    
+    @IBOutlet weak var searchName: UITextField!
+    
+    // Initialize variables
+    // Places - initialize core data entity
+    var places:Places? = nil
+    var context: NSManagedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+    var dataViewController: NSFetchedResultsController = NSFetchedResultsController()
+    
+    //let conText = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+    
+    //var nItem:Places? = nil
+
+    @IBAction func searchBegin(sender: UIButton) {
+        let fetchRequest = NSFetchRequest()
+        
+        // Create Entity Description
+        //let entityDescription = NSEntityDescription.entityForName("Places", inManagedObjectContext: context)
+        let entityDescription = NSEntityDescription.entityForName("Places", inManagedObjectContext: self.context)
+        
+        
+        // Configure Fetch Request
+        fetchRequest.entity = entityDescription
+        
+        do {
+            let result = try self.context.executeFetchRequest(fetchRequest)
+            //print(result)
+            
+            if (result.count > 0) {
+                for(var i = 0;i < result.count; i++)
+                {
+                    let person = result[i] as! NSManagedObject
+                    
+                    if let placenameStr = person.valueForKey("placeName"), placedescriptionStr = person.valueForKey("placeDescription") {
+                        
+                        if placenameStr.isEqual(self.searchName.text){
+                            print("love")
+                        }
+                        
+                        //print("\(placenameStr) \(placedescriptionStr)")
+                    }
+                    
+                }
+                //  print("1 - \(person)")
+                
+                
+                // print("2 - \(person)")
+            }
+            
+            
+        } catch {
+            let fetchError = error as NSError
+            print(fetchError)
+        }
+        
+    }
+    
+    
+    // getFetchResultsController() - get results returned from a Core Data fetch request
+    func getFetchResultsController() -> NSFetchedResultsController {
+        
+        dataViewController = NSFetchedResultsController(fetchRequest: listFetchRequest(), managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        
+        return dataViewController
+        
+    }
+    
+    // listFetchRequest() - list results returned from a Core Data fetch request
+    func listFetchRequest() -> NSFetchRequest {
+        
+        let fetchRequest = NSFetchRequest(entityName: "Places")
+        let sortDescripter = NSSortDescriptor(key: "placeName", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescripter]
+        return fetchRequest
+        
+    }
+    
+    // viewDidLoad() - loads view into the memory and does view initialization
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dataViewController = getFetchResultsController()
+        
+        dataViewController.delegate = self
+        do {
+            try dataViewController.performFetch()
+        } catch _ {
+        }
+        
+        // show the keyboard
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name:UIKeyboardWillShowNotification, object: nil);
+        
+        //hide the keyboard
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name:UIKeyboardWillHideNotification, object: nil);
+        
+    }
+    
+    // didReceiveMemoryWarning() - handles low memory conditions
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // move the view upwards as keyboard appears
+    func keyboardWillShow(sender: NSNotification) {
+        if(self.view.frame.origin.y >= 0) {
+            self.view.frame.origin.y -= 100
+        }
+    }
+    
+    // move the keyboard back as keyboard disapears
+    func keyboardWillHide(sender: NSNotification) {
+        if(self.view.frame.origin.y < 0) {
+            self.view.frame.origin.y += 100
+        }
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        view.endEditing(true)
+        self.searchName.resignFirstResponder()
+        
+        
+    }
+    
+
     
 }
+
